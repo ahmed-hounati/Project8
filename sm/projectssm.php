4d220251765216e36030d0935d96e7226b189c23
ahmed-hounati <hounatiahmed@gmail.com>
2023-12-13 14:44:35 +0100

    user

--- sm/projectssm.php
+++ sm/projectssm.php
@@ -2,7 +2,6 @@
 session_start();
 require '../includes/conn.inc.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,67 +13,69 @@ require '../includes/conn.inc.php';
 </head>
 
 <body>
+
     <div class="min-h-full">
-        <nav class="bg-gray-800">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="border-b border-gray-700">
-                    <div class="flex items-center justify-between h-16 px-4 sm:px-0">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0">
-                                <img class="w-[100px]" src="../img/logo-removebg-preview.png" alt="logo">
-                            </div>
-                            <div class="hidden md:block">
-                                <div class="ml-10 flex items-baseline space-x-4">
-                                    <!-- liens -->
-                                    <a href="./dashboardsm.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
-
-                                    <a href="./squads.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-
-                                    <a href="./projectssm.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-
-                                    <a href="../logout.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">logout</a>
+        <div class="pb-32">
+            <nav class="bg-gray-800">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="border-b border-gray-700">
+                        <div class="flex items-center justify-between h-16 px-4 sm:px-0">
+                            <div class="flex items-center">
+                                <div class="flex-shrink-0">
+                                    <img class="w-[100px]" src="../img/logo-removebg-preview.png" alt="logo">
                                 </div>
-                            </div>
-                        </div>
-                        <div class="hidden md:block">
-                            <div class="ml-4 flex items-center md:ml-6">
+                                <div class="hidden md:block">
+                                    <div class="ml-10 flex items-baseline space-x-4">
+                                        <!-- liens -->
+                                        <a href="./dashboardsm.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
 
+                                        <a href="./squads.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
 
-                                <!-- Profile dropdown -->
-                                <div class="ml-3 relative">
-                                    <div>
+                                        <a href="./projectssm.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+
+                                        <a href="../logout.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">logout</a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="-mr-2 flex md:hidden">
-                            <!-- Mobile menu button -->
-                            <button type="button" id="burger-menu" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                                <span class="sr-only">Open main menu</span>
-                                <!--
+                            <div class="hidden md:block">
+                                <div class="ml-4 flex items-center md:ml-6">
+
+
+                                    <!-- Profile dropdown -->
+                                    <div class="ml-3 relative">
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="-mr-2 flex md:hidden">
+                                <!-- Mobile menu button -->
+                                <button type="button" id="burger-menu" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                                    <span class="sr-only">Open main menu</span>
+                                    <!--
                     Heroicon name: outline/menu
 
                     Menu open: "hidden", Menu closed: "block"
                 -->
-                                <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
-                                <!--
+                                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    </svg>
+                                    <!--
                     Heroicon name: outline/x
 
                     Menu open: "block", Menu closed: "hidden"
                 -->
-                                <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
+                                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Mobile menu, show/hide based on menu state. -->
-            <div class="border-b border-gray-700 md:hidden" id="nav-links">
+                <!-- Mobile menu, show/hide based on menu state. -->
+                <div class="border-b border-gray-700 md:hidden" id="nav-links">
                 <div class="px-2 py-3 space-y-1 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                     <a href="./dashboardsm.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
@@ -85,28 +86,25 @@ require '../includes/conn.inc.php';
 
                     <a href="./login.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Login</a>
                 </div>
+                </div>
+            </nav>
+        </div>
 
-            </div>
-    </div>
-    </nav>
-    </div>
-
-    <main class="-mt-32">
-        <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-            <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-                <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
-                    <div class="flex justify-between">
-                        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Projects</h2>
-                    </div>
-                    <div class="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
-                        <?php
-                        $sql = "SELECT * FROM projects";
-                        try {
+        <main class="-mt-32">
+            <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+                <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+                    <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+                        <div class="flex justify-between">
+                            <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Projects</h2>
+                        </div>
+                        <div class="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+                            <?php
+                            $sql = "SELECT * FROM projects";
                             $stmt = $conn->query($sql);
                             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                             foreach ($rows as $row) {
-                        ?>
+                            ?>
                                 <div class="bg-gray-900 p-8 rounded-2xl">
                                     <a href="#" class="block mt-4">
                                         <p class="text-xl font-semibold text-white">Project ID: <?php echo $row['IDProject']; ?></p>
@@ -123,22 +121,19 @@ require '../includes/conn.inc.php';
                                         </div>
                                     </div>
                                 </div>
-                        <?php
+                            <?php
                             }
-                        } catch (PDOException $e) {
-                            echo "Error: " . $e->getMessage();
-                        } finally {
-                            // Close the PDO statement
-                            $stmt = null;
-                        }
-                        ?>
+                            if (isset($stmt)) {
+                                $stmt = null; // Set the PDO statement to null to free the result
+                            }
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
+        </main>
 
-    <script src="./js/script.js"></script>
+        <script src="./js/script.js"></script>
 </body>
 
 </html>
