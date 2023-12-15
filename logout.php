108e9bba2af671a13dcb0836fab0574108ae6f8d
ahmed-hounati <hounatiahmed@gmail.com>
2023-12-15 11:57:26 +0100

    logout

--- logout.php
+++ logout.php
@@ -1,10 +1,29 @@
 <?php
-session_start();
 
-$_SESSION = array();
+require_once './includes/conn.inc.php';
 
-session_destroy();
+class LogoutHandler
+{
+    private $conn;
 
-header("Location: ./index.php");
-exit();
-?>
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function logout()
+    {
+        // Destroy the session
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+
+        // Redirect to the login page
+        header("Location: ./index.php");
+        exit();
+    }
+}
+
+// Usage
+$logoutHandler = new LogoutHandler($conn);
+$logoutHandler->logout();
